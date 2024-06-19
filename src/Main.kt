197d78bdcd1353b6e76d891634a0796c6a972d41
Дмитрий Цугуй <dmitrijcuguj@Mac-mini-Dmitrij.local>
2024-06-19 16:35:39 +0500

    new

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+// Реализация с использованием изменяемого контейнера MutableSet
+fun removeDuplicatesWithSet(arr: IntArray): IntArray {
+    val seen = mutableSetOf<Int>()
+    val result = mutableListOf<Int>()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    arr.forEach { element ->
+        if (seen.add(element)) {
+            result.add(element)
+        }
     }
+
+    return result.toIntArray()
+}
+
+// Реализация с использованием цикла for и неизменяемого списка
+fun removeDuplicatesWithFor(arr: IntArray): IntArray {
+    val result = mutableListOf<Int>()
+
+    for (element in arr) {
+        if (element !in result) {
+            result.add(element)
+        }
+    }
+
+    return result.toIntArray()
+}
+
+fun main() {
+    val arr1 = intArrayOf(1, 2, 2, 3, 4, 4, 5)
+    val result1 = removeDuplicatesWithSet(arr1)
+    println("Решение с помощью MutableSet: ${result1.joinToString(", ")}")
+
+    val arr2 = intArrayOf(1, 2, 2, 3, 4, 4, 5)
+    val result2 = removeDuplicatesWithFor(arr2)
+    println("Решение с помощью for: ${result2.joinToString(", ")}")
 }
